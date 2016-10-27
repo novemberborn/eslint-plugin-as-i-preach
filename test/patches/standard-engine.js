43bda8dd2625977af317ebf72beb3e4c31ba68c7
Mark Wubben <mark@novemberborn.net>
2016-10-27 17:16:45 +0100

    Patch lintText to accept filename argument

--- test/patches/standard-engine.js
+++ test/patches/standard-engine.js
@@ -3,6 +3,80 @@ import test from 'ava'
 import engine from '../../lib/patched-standard-engine'
 import options from '../../options'
 
+test('lintText() takes a fourth filename argument', async t => {
+  const linter = engine.linter(Object.assign({}, options))
+  linter.parseOpts = opts => {
+    linter.eslint = {
+      CLIEngine (config) {
+        return {
+          executeOnText (text, filename) {
+            return { opts, config, text, filename }
+          }
+        }
+      }
+    }
+    return opts
+  }
+
+  const expectedText = 'some text'
+  const expectedConfig = {}
+  const expectedOpts = { eslintConfig: expectedConfig }
+  const expectedFilename = 'file.js'
+
+  const actual = await new Promise(resolve => {
+    linter.lintText(expectedText, expectedOpts, (_, result) => resolve(result), expectedFilename)
+  })
+
+  t.true(actual.opts === expectedOpts)
+  t.true(actual.config === expectedConfig)
+  t.true(actual.text === expectedText)
+  t.true(actual.filename === expectedFilename)
+})
+
+test('lintText() takes a third filename argument if there are no options', async t => {
+  const expectedConfig = {}
+
+  const linter = engine.linter(Object.assign({}, options))
+  linter.parseOpts = opts => {
+    linter.eslint = {
+      CLIEngine (config) {
+        return {
+          executeOnText (text, filename) {
+            return { opts, config, text, filename }
+          }
+        }
+      }
+    }
+    return { eslintConfig: expectedConfig }
+  }
+
+  const expectedText = 'some text'
+  const expectedFilename = 'file.js'
+
+  const actual = await new Promise(resolve => {
+    linter.lintText(expectedText, (_, result) => resolve(result), expectedFilename)
+  })
+
+  t.true(actual.opts === null)
+  t.true(actual.config === expectedConfig)
+  t.true(actual.text === expectedText)
+  t.true(actual.filename === expectedFilename)
+})
+
+test('lintText() propagates linting exceptions', async t => {
+  const linter = engine.linter(Object.assign({}, options))
+  const expected = new Error()
+  linter.eslint = {
+    CLIEngine () { throw expected }
+  }
+
+  const result = new Promise((resolve, reject) => {
+    linter.lintText('some text', reject)
+  })
+  const err = await t.throws(result)
+  t.true(err === expected)
+})
+
 test('parseOpts() applies settings', t => {
   const cwd = join(__dirname, '..', 'fixtures', 'settings')
   const linter = engine.linter(Object.assign({}, options, { cwd }))
